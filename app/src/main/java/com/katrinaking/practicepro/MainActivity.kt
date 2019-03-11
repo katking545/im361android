eb94fd9e1612c67b2478ea52488ec2858fb9fb7a
Katrina King <kmking@mail.bradley.edu>
2019-03-10 19:27:47 -0500

    Initial commit

---
+++ app/src/main/java/com/katrinaking/practicepro/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.katrinaking.practicepro
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var myText: TextView = findViewById(R.id.my_text_view)
+        var myButton: Button = findViewById(R.id.my_button)
+        myButton.setOnClickListener {
+            myText.text = "I've been clicked!"
+        }
+    }
+}
