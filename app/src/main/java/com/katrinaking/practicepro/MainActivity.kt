f42f87f179554084b4c80e124be28d0c0460f560
Katrina King <kmking@mail.bradley.edu>
2019-03-24 17:32:40 -0500

    layout

--- app/src/main/java/com/katrinaking/practicepro/MainActivity.kt
+++ app/src/main/java/com/katrinaking/practicepro/MainActivity.kt
@@ -12,11 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myText: TextView = findViewById(R.id.my_text_view)
-        val myButton: Button = findViewById(R.id.my_button)
-        myButton.setOnClickListener {
-            counter++
-            myText.text = resources.getQuantityString(R.plurals.has_been_clicked, counter, counter)
-        }
+
     }
 }
