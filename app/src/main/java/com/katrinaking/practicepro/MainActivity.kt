faab1cbd807ae68e3c79fb6ee400ad8eda6fbdd7
Katrina King <kmking@mail.bradley.edu>
2019-03-25 18:47:20 -0500

    class

--- app/src/main/java/com/katrinaking/practicepro/MainActivity.kt
+++ app/src/main/java/com/katrinaking/practicepro/MainActivity.kt
@@ -2,10 +2,18 @@ package com.katrinaking.practicepro
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onMapReady(map: GoogleMap?) {
+        Log.i("PracticePro", "Map is ready")
+    }
+
     var counter = 0
 
 
@@ -13,5 +21,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.my_map_view) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
 }
