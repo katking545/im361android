124dd10058ddaf8a777a166210903d919ec44601
Katrina King <kmking@mail.bradley.edu>
2019-04-07 22:17:37 -0500

    Getting closer

--- app/src/main/java/com/katrinaking/practicepro/DashActivity.kt
+++ app/src/main/java/com/katrinaking/practicepro/DashActivity.kt
@@ -20,7 +20,19 @@ class DashActivity : AppCompatActivity() {
 
         val btnStartActivity: Button = findViewById(R.id.button4)
         btnStartActivity.setOnClickListener {
+            val intent = Intent(this, PracticeActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnLessonActivity: Button = findViewById(R.id.button5)
+        btnLessonActivity.setOnClickListener {
             val intent = Intent(this, LessonActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnToolsActivity: Button = findViewById(R.id.button6)
+        btnToolsActivity.setOnClickListener {
+            val intent = Intent(this, ToolsActivity::class.java)
             startActivity(intent)
         }
 
